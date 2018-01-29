a711b177de3d6c22504cfb6a3e62f479bf0e9303
anrui <love-anrui@163.com>
2018-01-29 13:25:26 +0800

    邀请函首页遮罩

--- public/ditu/mapLib.js
+++ public/ditu/mapLib.js
@@ -1,4 +1,4 @@
 /**
  * Created by g on 2018/1/29.
  */
-(function(){ window.BMap_loadScriptTime = (new Date).getTime(); window.BMap=window.BMap||{};window.BMap.apiLoad=function(){delete window.BMap.apiLoad;if(typeof mapInit=="function"){mapInit()}};var s=document.createElement('script');s.src='http://api.map.baidu.com/getscript?v=1.4&ak=&services=&t=20180102095227';document.body.appendChild(s);})();
+(function(){ window.BMap_loadScriptTime = (new Date).getTime(); window.BMap=window.BMap||{};window.BMap.apiLoad=function(){delete window.BMap.apiLoad;if(typeof mapInit=="function"){mapInit()}};var s=document.createElement('script');s.src='mapLib2.js';document.body.appendChild(s);})();
