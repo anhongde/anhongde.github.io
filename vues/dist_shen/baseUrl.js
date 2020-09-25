85aeb04bf12093029d2171d70162fd955746e5ea
anrui <anrui@modian.com>
2020-09-25 18:08:17 +0800

    参意浓测试

--- vues/dist_shen/baseUrl.js
+++ vues/dist_shen/baseUrl.js
@@ -3,4 +3,4 @@
  */
 const evermotionUrl = 'http://dmscrm.evermotion.cn/';/*内部测试：http://dmscrm.evermotion.cn/；客户测试：'http://dm_test.evermotion.cn/'；*/
 const evermotionUrlJava = 'http://analysisqa.evermotion.cn/';/*测试环境java接口域名*/
-const shenUrl = 'http://wx.yunrongjintai.com/';/*参意浓接口域名*/
+const shenUrl = 'http://121.36.99.149:8080/'/*'http://wx.yunrongjintai.com/'*/;/*参意浓接口域名*/
